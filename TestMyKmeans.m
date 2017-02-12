c1718970da6b4dc83ca8ccb5c5aa81b044ee45d9
Omer Baldo <omer.s.baldo@gmail.com>
2017-02-12 16:45:07 -0500

    Update TestMyKmeans.m

--- TestMyKmeans.m
+++ TestMyKmeans.m
@@ -36,26 +36,58 @@ function TestMyKmeans(filename,numRepeat, numIter)
         feval('save',[filename '.summary.txt'],'output','-ascii');
  end
      
-    
+% Graph Times
 figure;
-plot(1:numIter,SD1,'linewidth',1);hold on; grid on;
+plot(1:numIter,SD1,'linewidth',2);hold on; grid on;
 set(gca,'FontSize',20);
 xlabel('Iteration');ylabel('SD');
 title(filename);
 
-
+% Graph Accuracies
 figure;
-plot(1:numIter,acc1,'linewidth',1);hold on; grid on;
-set(gca,'FontSize',20x);
+plot(1:numIter,acc1,'-o','linewidth',2);hold on; grid on;
+set(gca,'FontSize',20);
 xlabel('Iteration');ylabel('Accuracy (%)');
 title(filename);
 
-
-
+% average line
+averages = zeros(1,numIter);
+for i=1:numIter
+    averages(1,i) = mean(acc1(i,:));
+end 
+plot(1:numIter,averages,'k--p', 'linewidth',3, 'linestyle','--');hold on; grid on;
 
 
 
 figure;
+ylabel('Maltab Kmeans Time');xlabel('My Kmeans Time');hold on;
+title(filename);hold on;
+
+% get max of each one 
+max1 = T1(1); % X
+max2 = T2(1); % Y
+for i=1:numRepeat
+    if T1(i)>max1
+        max1 = T1(i)
+    end
+    if T2(i)>max2
+        max2 = T1(i)
+    end
+end
+
+axis([0,max1,0,max2 ]); hold on;
+x = linspace(0,max1);hold on;
+y = x;hold on;
+plot(x,y,'r', 'linestyle','--');hold on;
+hold on;
+
+for i=1:numRepeat
+    plot(T1(1,i),T2(1,i),'b--d');
+    hold on;
+
+end
+
+
 %%%%%%%%
 %%%% Plot times
 %%%%%%%%
