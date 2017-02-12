1d1a1e1074737674e9a07c28d11d9cf485f3d8ac
Omer Baldo <omer.s.baldo@gmail.com>
2017-02-12 13:18:28 -0500

    Update TestMyKmeans.m

--- TestMyKmeans.m
+++ TestMyKmeans.m
@@ -13,18 +13,26 @@ function TestMyKmeans(filename,numRepeat, numIter)
     for i = 1:numRepeat
         i
         C0 = X(randsample(n,K),:);
+        % SD1(i,:)
         tic;[idx1{i},C1,SD1(i,:),D1{i}]=MyKmeans(X,K,C0,numIter); T1(i) = toc;
         tic;[idx2{i},C2,sumd2,D2{i}]=kmeans(full(X),K,'Start',full(C0),'Maxiter',numIter);
         T2(i)=toc;
         SD2(i,:) = sum(sumd2);
-        for t = 1:numIter
-            acc1(i,t) = evalClust_Error(idx1{i}(t,:),Y);
-        end
 
-        %%%%%%%
-        %%%% Evluate the classification accuracy for Maltab Kmeans algorithm
-        %%%%%%%%
+        % Mat Lab K Means
+        idx2_RowForm = idx2{i}(:,1);
+        idx2_RowForm = idx2_RowForm';
+        acc2(1,i) = evalClust_Error(idx2_RowForm,Y);
+        
+        % My Kmeans 
+        for t = 1:numIter
+           acc1(i,t) = evalClust_Error(idx1{i}(t,:),Y);
+        end
+      
+        disp('');
         output = [acc1(1:i,end) acc2(1:i)' SD1(1:i,end) SD2(1:i) T1(1:i)' T2(1:i)'];
+        
+        
         feval('save',[filename '.summary.txt'],'output','-ascii');
  end
      
@@ -34,10 +42,19 @@ plot(1:numIter,SD1,'linewidth',1);hold on; grid on;
 set(gca,'FontSize',20);
 xlabel('Iteration');ylabel('SD');
 title(filename);
+
+
 figure;
-%%%%%%
-%%% Plot accuracies
-%%%%
+plot(1:numIter,acc1,'linewidth',1);hold on; grid on;
+set(gca,'FontSize',20x);
+xlabel('Iteration');ylabel('Accuracy (%)');
+title(filename);
+
+
+
+
+
+
 figure;
 %%%%%%%%
 %%%% Plot times
