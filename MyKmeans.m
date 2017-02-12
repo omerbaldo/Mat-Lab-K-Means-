4a7997a4134303f0aef698708162c2031b1f4f83
Omer Baldo <omer.s.baldo@gmail.com>
2017-02-12 13:18:57 -0500

    Update MyKmeans.m

--- MyKmeans.m
+++ MyKmeans.m
@@ -1,12 +1,18 @@
-function [idx,C,sumD,D] = MyKmeansv2(X,K,C0,numIter)
+function [idx,C,SD,D] = MyKmeans(X,K,C0,numIter)
+    
+      SD = zeros(1,numIter);   % sum of sumD at each iteration 
       rows = size(X,1);        % number of rows 
       cols = size(X,2);        % number of columns
-      C = C0;                    % k x p matrix which has cluster centers
+      C = C0;                  % k x p matrix which has cluster centers
       sumD = zeros(K,1);       % k x 1 column vector sum of how far all points in a group are to the center 
       idx = zeros(rows,1);     % n x 1 column vector containing row indexes for center 
       D = zeros(rows,K);       % n x k matrix containing distances for each point to every center 
+      idxPerIteration = zeros(numIter,rows);
+
       
       for i=1:numIter
+
+          ixxxx = i
           for p= 1:rows                        
             min_distance_index = 1;          
             min_distance = sum((C(1,:)-X(p,:)).^2);    
@@ -39,10 +45,25 @@ function [idx,C,sumD,D] = MyKmeansv2(X,K,C0,numIter)
               C(elem,:) = C(elem,:)/ member_amount(elem,1);
           end
           
-      end
+          
+         for i=1:rows      % computer SUMD 
+           sumD(idx(i,1), 1) = sumD(idx(i,1), 1) + D(i,idx(i,1));
+         end
+         
+         ss = 0
+         for i=1:K
+           ss = ss + sumD(i,1);
+         end 
+         SD(1,ixxxx) = ss;
+         
+         idxPerIteration(ixxxx,:) = idx';
+         disp('dd');
+          
+      end % End Loop
 
            
-      for p= 1:rows                        
+     
+      for p= 1:rows             % Recompute Distances                        
             min_distance = sum((C(1,:)-X(p,:)).^2);    
             for center= 1:K                   
               distance = sum((C(center,:)-X(p,:)).^2);                    
@@ -57,4 +78,18 @@ function [idx,C,sumD,D] = MyKmeansv2(X,K,C0,numIter)
       for i=1:rows      % computer SUMD 
            sumD(idx(i,1), 1) = sumD(idx(i,1), 1) + D(i,idx(i,1));
       end 
+      
+      ss = 0
+      for i=1:K
+          ss = ss + sumD(i,1);
+      end 
+      SD(1,numIter) = ss;
+
+      
+      % idx flipped
+      
+      idx = idxPerIteration;
+      
+      
+      
 end % end function
